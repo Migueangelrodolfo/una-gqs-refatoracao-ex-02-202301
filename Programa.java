ce6dca2e4fede791f497b9d352aa8e7ef3ffb431
Migueangelrodolfo <92491142+Migueangelrodolfo@users.noreply.github.com>
2023-06-12 19:54:19 -0300

    Update Programa.java

--- Programa.java
+++ Programa.java
@@ -6,43 +6,20 @@ public class Programa {
 
     	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		ValidacaoIdadeHomem validacaoIdadeHomem = new ValidacaoIdadeHomem();
+		ValidacaoIdadeMulher validacaoIdadeMulher = new ValidacaoIdadeMulher();
 
-		System.out.print("Informe o sexo do cliente (M/F): ");
-		String sexo = scanner.next();
 
-    		double mensalidade =0;
+		validacaoIdadeHomem.idadeValidacao();
+		validacaoIdadeMulher.idadeValidacao();
+
+
 
    if (sexo.equalsIgnoreCase("M")) {
-			if (idade <= 15) {
-				mensalidade = 60.0;
-			} else if (idade <= 18) {
-				mensalidade = 75.0;
-			} else if (idade <= 30) {
-				mensalidade = 90.0;
-			} else if (idade <= 40) {
-				mensalidade = 85.0;
-			} else if(idade >= 41 && idade <= 50){
-				mensalidade = 80.0;
-			} else if (idade >= 51 && idade <=125) {
-				mensalidade = 60.0;
-			} else if (idade <= 0 || idade >= 126) {
-				System.out.println("Idade invalida");
-			}
+		
 
 		} else if (sexo.equalsIgnoreCase("F")) {
-			if (idade <= 18) {
-				mensalidade = 60.0;
-			} else if (idade <= 25) {
-				mensalidade = 90.0;
-			} else if (idade <= 40) {
-				mensalidade = 85.0;
-			} else if (idade <= 50) {
-				mensalidade = 65.0;
-			} else if (idade >= 50 && idade <= 120) {
-				mensalidade = 50.0;
-			} else if (idade <= 0 || idade >= 125) {
-				System.out.println("Idade invalida");
-			}
+			
 		} else {
 			System.out.println("Sexo inválido.");
 			mensalidade = 0.0;
